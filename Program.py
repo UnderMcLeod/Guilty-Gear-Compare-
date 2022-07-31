d9e8b8c32572f1ad58f7f4918d15fcc63b0d53c9
Dawson M Arrington <96447040+UnderMcLeod@users.noreply.github.com>
2022-07-31 18:20:35 -0500

    Add files via upload
    The first version will have 3 characters to choose from

---
+++ Program.py
@@ -0,0 +1,17 @@
+
+print("Hello fellow Gear. ")
+string=input("enter a number between 1 to 3:")
+match string:
+        case "1 for Sol":
+             print("Gorilla")
+
+        case "2 for Ky":
+            print('Pretty boy')
+
+        case "if you a bad witch Press 3":
+            print("Stroke that tree baby")
+        case unknown_command:
+            print("unkknown command: {unknown_command")
+            
+ #this section is the menu. Starting off with 3 characters. will add more if i feel like it. 
+ # switch case. I made a similar project like this in pg2.I was getting erros due to spacing
